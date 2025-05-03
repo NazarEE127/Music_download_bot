522af67222989652700b5a4f6123d8ea5a95a3e3
NazarEE127 <nazarfedotko50@gmail.com>
2025-05-03 18:19:05 +0700

    first commit
    создал базовую структуру проекта

---
+++ api.py
@@ -0,0 +1,39 @@
+from flask_restful import reqparse, abort, Api, Resource
+from flask import Flask, render_template, request, redirect, flash
+
+app = Flask(__name__)
+api = Api(app)
+
+def abort_if_news_not_found(news_id):
+    session = db_session.create_session()
+    news = session.query(News).get(news_id)
+    if not news:
+        abort(404, message=f"News {news_id} not found")
+
+class NewsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(News).all()
+        return jsonify({'news': [item.to_dict(
+            only=('title', 'content', 'user.name')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = News(
+            title=args['title'],
+            content=args['content'],
+            user_id=args['user_id'],
+            is_published=args['is_published'],
+            is_private=args['is_private']
+        )
+        session.add(news)
+        session.commit()
+        return jsonify({'id': news.id})
+
+
+# для списка объектов
+api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+
+# для одного объекта
+api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
